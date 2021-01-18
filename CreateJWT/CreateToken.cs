0504d4dcb1ae3dae4b2c5b9f3d5ddffd9235f264
Fabio Costa <fabio.costa@transferoswiss.ch>
2021-01-18 11:33:21 -0300

    first commit

---
+++ CreateJWT/CreateToken.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ManuallyCreateJWT.CreateJWT
+{
+    public class CreateToken
+    {
+        public string GenerateTokenV2(int userId)
+        {
+            string privateKeyFile = File.ReadAllText(@"Path to private key");
+            byte[] privateKey = Convert.FromBase64String(privateKeyFile);
+            using RSA rsa = RSA.Create();
+            rsa.ImportPkcs8PrivateKey(privateKey, out _);
+
+            var signingCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256)
+            {
+                CryptoProviderFactory = new CryptoProviderFactory { CacheSignatureProviders = false }
+            };
+
+            var myIssuer = "http://mysite.com";
+            var myAudience = "http://myaudience.com";
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                Issuer = myIssuer,
+                Audience = myAudience,
+                SigningCredentials = signingCredentials
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public bool ValidateCurrentTokenV2(string token)
+        {
+            string publicKeyFile = File.ReadAllText(@"Path to public key");
+            byte[] publicKey = Convert.FromBase64String(publicKeyFile);
+
+            using RSA rsa = RSA.Create();
+            rsa.ImportSubjectPublicKeyInfo(publicKey, out _);
+
+            //var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
+
+            var myIssuer = "http://mysite.com";
+            var myAudience = "http://myaudience.com";
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = myIssuer,
+                    ValidAudience = myAudience,
+                    IssuerSigningKey = new RsaSecurityKey(rsa)
+                }, out SecurityToken validatedToken);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ManuallyCreateJWT.CreateJWT;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ManuallyCreateJWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var createToken = new CreateToken();
+            string token = createToken.GenerateTokenV2(123);
+            Console.WriteLine(token);
+            Console.WriteLine(createToken.ValidateCurrentTokenV2(token));
+        }
+    }
+}
